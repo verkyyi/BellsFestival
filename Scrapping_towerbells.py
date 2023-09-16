06a390a0fbe93f8b2c89fdb497d4fd24f43388b2
jackyzhang1008 <jackyzhang1008@gmail.com>
2023-09-16 14:04:07 -0400

    Update Scrapping_towerbells.py

--- Scrapping_towerbells.py
+++ Scrapping_towerbells.py
@@ -99,17 +99,6 @@ def parse_data_page(
 
     return sections
 
-def get_contact(contract_part):
-    dic = {}
-    address_matches = re.search(r'(?<=\()(A)(?=\))([\s\S]*?)T:', contract_part)
-    if address_matches:
-        address = address_matches.group(2).strip().replace('\n', ', ').replace('   ', ' ')
-        dic['Address'] = address
-    phone_matches = re.search(r'T: (\(\d{3}\)\d{3}-?\d{4})', contract_part)
-    if phone_matches:
-        phone = phone_matches.group(1)
-        dic['Telephone:'] = phone
-    return dic
 
 
 
@@ -153,7 +142,17 @@ def get_fields_section_name(section_text):
 # Section Past carillonist
 
 # Section Contact
-
+def get_contact(contract_part):
+    dic = {}
+    address_matches = re.search(r'(?<=\()(A)(?=\))([\s\S]*?)T:', contract_part)
+    if address_matches:
+        address = address_matches.group(2).strip().replace('\n', ', ').replace('   ', ' ')
+        dic['Address'] = address
+    phone_matches = re.search(r'T: (\(\d{3}\)\d{3}-?\d{4})', contract_part)
+    if phone_matches:
+        phone = phone_matches.group(1)
+        dic['Telephone:'] = phone
+    return dic
 # Section Schedule
 
 # Section Remarks
